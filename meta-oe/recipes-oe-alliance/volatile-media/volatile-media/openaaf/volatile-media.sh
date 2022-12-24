4ebede7ff31a5f2c26e0801495c95b052136428d
titanobi <_obiwan_@web.de>
2022-12-24 14:41:44 +0100

    test

---
+++ meta-oe/recipes-oe-alliance/volatile-media/volatile-media/openaaf/volatile-media.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+if [ -e /media/settings ];then
+	cp -a /media/settings /etc/titan.restore/mnt
+fi
+mountpoint -q "/media" || mount -t tmpfs -o size=64k tmpfs /media
+
+create_point() {
+	if [ ! -d $2 ]
+	then
+		mkdir -p $2
+	fi
+}
+
+grep -v "^#" /etc/fstab | grep "\s/media/" | while read LINE
+do
+	create_point $LINE
+done
+
+if [ ! -e /dev/fuse ]
+then
+	modprobe fuse
+fi
